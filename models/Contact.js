91355eb0efb2fe95326956c287daff1437164a1d
phanendraguptha <www.phani122@gmail.com>
2020-05-28 20:48:00 +0530

    changed database schema

--- models/Contact.js
+++ models/Contact.js
@@ -4,10 +4,20 @@ require('mongoose-type-email');
 var contactSchema = mongoose.Schema({
   name: String,
   dob: String,
-  phone1: Number,
-  phone2: { type: Number, allowBlank: true },
+  phone1: {
+    type: String,
+    unique: true
+  },
+  phone2: {
+    type: Number,
+    unique: true,
+    allowBlank: true
+  },
   email1: mongoose.SchemaTypes.Email,
-  email2: { type: mongoose.SchemaTypes.Email, allowBlank: true }
+  email2: {
+    type: mongoose.SchemaTypes.Email,
+    allowBlank: true
+  }
 });
 
 var Contact = mongoose.model("Contact", contactSchema);
